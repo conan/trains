e72a3bf6cc48b75bedc5ce27d24b4e2f59e0c587
Conan Cook <cook.conan@gmail.com>
2013-02-24 20:22:24 +0000

    Working DFS for number of paths count, BFS floating about

--- src/main/java/trains/RouteInformation.java
+++ src/main/java/trains/RouteInformation.java
@@ -1,15 +1,195 @@
 package trains;
 
-import java.util.List;
 
-public interface RouteInformation {
+import java.util.*;
 
-    public void setGraph(int[][] graph);
+public class RouteInformation {
 
-    public String distance(List<Integer> route);
+    // Calculated predecessor tree
+    private Integer[] predecessor;
 
-    public int numberOfRoutes(int source, int destination);
+    // Shortest path estimates from the source vertex
+    private Integer[] estimate;
 
-    public List<Integer> shortestRoute(int source, int destination);
+    /**
+     * Implementation of Dijkstra's algorithm for calculating single-source shortest-path distances.
+     *
+     * @param graph     adjacency matrix of input graph
+     * @param source    source vertex
+     */
+    private void dijkstra(Integer[][] graph, int source) {
+        predecessor = new Integer[graph.length];
+        estimate = new Integer[graph.length];
+
+        // Set initial estimates to maximum
+        for (int v = 0; v < graph.length; v++) {
+            estimate[v] = Integer.MAX_VALUE;
+        }
+
+        // Min-priority queue keyed on estimate values.
+        // This is a binary heap; a Fibonacci heap would be better, but there isn't currently one in java.util
+        Queue<Integer> toDo = new PriorityQueue<>(graph.length,
+                new Comparator<Integer>() {
+                    @Override
+                    public int compare(Integer u, Integer v) {
+                        return estimate[u].compareTo(estimate[v]);
+                    }
+                }
+        );
+
+        // Start with source vertex
+        toDo.add(source);
+
+        while (!toDo.isEmpty()) {
+            // Get lowest-estimate vertex from queue
+            int u = toDo.remove();
+
+            // Relax all adjacent vertices
+            for (int v = 0; v < graph[u].length; v++) {
+                if(graph[u][v] != null) {
+                    if(u == source) {
+                        // This is a hack to accomodate that the shortest estimate from one vertex to itself cannot be 0
+                        estimate[v] = graph[u][v];
+                        predecessor[v] = u;
+                        toDo.add(v);
+                    } else if(estimate[v] > estimate[u] + graph[u][v]){
+                        estimate[v] = estimate[u] + graph[u][v];
+                        predecessor[v] = u;
+                        toDo.add(v);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * "Relaxes" the shortest path estimate of edge (u, v). (see CLRS 2001, MIT, ch. 24)
+     *
+     * @param u      the start vertex
+     * @param v      the end vertex
+     * @param weight the weight of the edge (u, v)
+     */
+    private void relax(int u, int v, int weight, Queue<Integer> queue) {
+        if (estimate[v] > estimate[u] + weight) {
+            estimate[v] = estimate[u] + weight;
+            predecessor[v] = u;
+        }
+    }
+
+    private void breadthFirstSearch(Integer[][] graph, int source, int destination) {
+        // Internal data structures
+        Queue<Integer> queue = new LinkedList<>();
+        Colour[] colour = new Colour[graph.length];
+
+        // Output
+        predecessor = new Integer[graph.length];
+        estimate = new Integer[graph.length];
+
+        for (int i = 0; i < graph.length; i++) {
+            colour[i] = Colour.WHITE;
+        }
+        colour[source] = Colour.GREY;
+        estimate[source] = 0;
+        queue.add(source);
+        while (!queue.isEmpty()) {
+            int u = queue.remove();
+            for (int v = 0; v < graph[u].length; v++) {
+                if(graph[u][v] != null) {
+                    if (v != u) {
+                        if (colour[v] == Colour.WHITE) {
+                            colour[v] = Colour.GREY;
+                            estimate[v] = estimate[u] + 1;
+                            predecessor[v] = u;
+                            queue.add(v);
+                        }
+                    }
+                    if (v == destination) {
+                        int p = v;
+                        Stack<Integer> path = new Stack<>();
+                        while (predecessor[p] != source) {
+                            path.push(p);
+                            p = predecessor[p];
+                        }
+                        while(!path.isEmpty()) {
+                            System.out.print(path.pop());
+                        }
+                        System.out.println("\n");
+                    }
+                }
+            }
+            colour[u] = Colour.BLACK;
+        }
+    }
+
+    /**
+     * Placeholder for marking vertices in BFS as the graph is explored:
+     * <ul>
+     * <li>WHITE: undiscovered</li>
+     * <li>GREY: discovered, some adjacent vertices not explored</li>
+     * <li>BLACK: discovered and all adjacent vertices explored</li>
+     * </ul>
+     */
+    private enum Colour {
+        WHITE, GREY, BLACK;
+    }
+
+    public String distance(Integer[][] graph, List<Integer> route) {
+        int routeLength = 0;
+        for (int i = 0; i < route.size() - 1; i++) {
+            // Get next edge weight
+            int u = route.get(i);
+            int v = route.get(i + 1);
+            Integer edge = graph[u][v];
+
+            // If there's no edge, fail
+            if (edge == null) {
+                return "NO SUCH ROUTE";
+            }
+
+            routeLength += graph[u][v];
+        }
+        return String.valueOf(routeLength);
+    }
+
+    public Integer shortestRouteLength(Integer[][] graph, int source, int destination) {
+        dijkstra(graph, source);
+        return estimate[destination];
+    }
+
+    int count = 0;
+    Stack<Integer> path = new Stack<>();
+
+    public Integer numberOfRoutesWithMaxLength(Integer[][] graph, int source, int destination, int maxLength) {
+        return countRoutes(graph, source, destination, maxLength);
+    }
+
+    private int countRoutes(Integer[][] graph, int u, int destination, int maxLength) {
+        int count = 0;
+
+        // Add this node to path
+        path.push(u);
+
+        // If it's the destination then count it, but not if we haven't visited any other vertices
+        if(u == destination && path.size() > 1) {
+            count++;
+        }
+
+        // If we've hit the max path length, retreat
+        if (path.size() - 1 == maxLength) {
+            path.pop();
+            return count;
+        }
+
+        // Examine adjacent nodes
+        for(int v=0; v<graph[u].length; v++) {
+            if(graph[u][v] != null) {
+                count += countRoutes(graph, v, destination, maxLength);
+            }
+        }
+
+        // Retreat
+        path.pop();
+        return count;
+    }
 
 }
--- src/test/java/trains/RouteInformationTest.java
+++ src/test/java/trains/RouteInformationTest.java
@@ -1,33 +1,31 @@
 package trains;
 
-import junit.framework.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
-import static trains.RouteInformation.*;
+import static org.junit.Assert.assertEquals;
 
 
 public class RouteInformationTest {
 
     // Vertex constants to make this more readable
-    public static final int A = 1;
-    public static final int B = 2;
-    public static final int C = 3;
-    public static final int D = 4;
-    public static final int E = 5;
+    public static final int A = 0;
+    public static final int B = 1;
+    public static final int C = 2;
+    public static final int D = 3;
+    public static final int E = 4;
 
-    private int[][] testGraph;
+    private Integer[][] testGraph;
 
     private RouteInformation routeInformation;
 
-    @BeforeClass
+    @Before
     public void setUp(){
         // Graph as defined in problem spec
-        testGraph = new int[5][5];
+        testGraph = new Integer[5][5];
         testGraph[A][B] = 5;
         testGraph[B][C] = 4;
         testGraph[C][D] = 8;
@@ -39,92 +37,115 @@ public class RouteInformationTest {
         testGraph[A][E] = 7;
 
         // Class under test
-        //routeInformation = new BFSRouteInformation();
+        routeInformation = new RouteInformation();
     }
 
     /**
      * 1. The distance of the route A-B-C.
      */
     @Test
-    public void testOne() {
+    public void test1() {
         List<Integer> route = new ArrayList<>();
         route.add(A);
         route.add(B);
         route.add(C);
-        String result = routeInformation.distance(route);
-        assertEquals("Incorrect result for test #1 (route A-B-C)", "9", result);
+        String result = routeInformation.distance(testGraph, route);
+        assertEquals("Incorrect result for route A-B-C", "9", result);
     }
 
     /**
      * 2. The distance of the route A-D.
      */
     @Test
-    public void testTwo() {
+    public void test2() {
         List<Integer> route = new ArrayList<>();
         route.add(A);
         route.add(D);
-        String result = routeInformation.distance(route);
-        assertEquals("Incorrect result for test #2 (route A-D)", "5", result);
+        String result = routeInformation.distance(testGraph, route);
+        assertEquals("Incorrect result for route A-D", "5", result);
     }
 
     /**
      * 3. The distance of the route A-D-C.
      */
     @Test
-    public void testThree() {
+    public void test3() {
         List<Integer> route = new ArrayList<>();
         route.add(A);
         route.add(D);
         route.add(C);
-        String result = routeInformation.distance(route);
-        assertEquals("Incorrect result for test #3 (route A-D-C)", "13", result);
+        String result = routeInformation.distance(testGraph, route);
+        assertEquals("Incorrect result for route A-D-C", "13", result);
     }
 
     /**
      * 4. The distance of the route A-E-B-C-D.
      */
     @Test
-    public void testFour() {
+    public void test4() {
         List<Integer> route = new ArrayList<>();
         route.add(A);
         route.add(E);
         route.add(B);
         route.add(C);
         route.add(D);
-        String result = routeInformation.distance(route);
-        assertEquals("Incorrect result for test #4 (route A-E-B-C-D)", "22", result);
+        String result = routeInformation.distance(testGraph, route);
+        assertEquals("Incorrect result for route A-E-B-C-D", "22", result);
     }
 
     /**
      * 5. The distance of the route A-E-D.
      */
     @Test
-    public void testFive() {
+    public void test5() {
         List<Integer> route = new ArrayList<>();
         route.add(A);
         route.add(E);
         route.add(D);
-        String result = routeInformation.distance(route);
-        assertEquals("Incorrect result for test #2 (route A-D)", "NO SUCH ROUTE", result);
+        String result = routeInformation.distance(testGraph, route);
+        assertEquals("Incorrect result for route A-E-D", "NO SUCH ROUTE", result);
     }
 
     /**
      * 6. The number of trips starting at C and ending at C with a maximum of 3 stops.
      */
     @Test
-    public void testSix() {
-
+    public void test6() {
+        int result = routeInformation.numberOfRoutesWithMaxLength(testGraph, C, C, 3);
+        assertEquals("Incorrect result for no of routes C-C max length 3", 2, result);
     }
 
     /**
      * 7. The number of trips starting at A and ending at C with exactly 4 stops.
      */
     @Test
-    public void testSeven() {
+    public void test7() {
 
     }
 
     /**
-     *
+     * 8. The length of the shortest route (in terms of distance to travel) from A to C.
      */
+    @Test
+    public void test8() {
+        int distance = routeInformation.shortestRouteLength(testGraph, A, C);
+        assertEquals("Incorrect result for shortest route length A-C", 9, distance);
+    }
+
+    /**
+     * 9. The length of the shortest route (in terms of distance to travel) from B to B.
+     */
+    @Test
+    public void test9() {
+        int distance = routeInformation.shortestRouteLength(testGraph, B, B);
+        assertEquals("Incorrect result for shortest route length A-C", 9, distance);
+    }
+
+    /**
+     * 10. The number of different routes from C to C with a distance of less than 30.
+     */
+    @Test
+    public void test10() {
+
+    }
 }
